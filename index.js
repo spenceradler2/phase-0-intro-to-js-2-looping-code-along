533c4ceb7cfbf6e6e2b54ed54eb4f7192978ac18
Spencer <spencer.adler2@gmail.com>
2024-04-19 21:10:26 -0400

    Complete Loop Lab

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+function writeCards(peopleName, eventName) {
+  const thankYou = []
+  for (let i = 0; i < peopleName.length; i++) {
+    thankYou.push(`Thank you, ${peopleName[i]}, for the wonderful ${eventName} gift!`);
+    
+  }
+
+  return thankYou;
+}
+
+function countDown(count) {
+ // the initialization moves OUTSIDE the body of the loop!
+  while (count >= 0 ) {
+    console.log(count--);
+     // the iteration moves INSIDE the body of the loop!
+  }
+}
